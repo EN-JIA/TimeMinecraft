4ff8d89d275835f6fd08fa567a7f38cc0068f06a
grumm9472 <grumm9472@163.com>
2022-03-23 14:11:20 +0800

    texture amd tag update

--- src/main/java/enchia/time/main/block/ClassicGoldenBlockBlock.java
+++ src/main/java/enchia/time/main/block/ClassicGoldenBlockBlock.java
@@ -38,7 +38,7 @@ public class ClassicGoldenBlockBlock extends TimeModElements.ModElement {
 
 	public static class CustomBlock extends Block {
 		public CustomBlock() {
-			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(5f, 10f).setLightLevel(s -> 0).harvestLevel(2)
+			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(5f, 10f).setLightLevel(s -> 5).harvestLevel(2)
 					.harvestTool(ToolType.PICKAXE).setRequiresTool());
 			setRegistryName("classic_golden_block");
 		}
